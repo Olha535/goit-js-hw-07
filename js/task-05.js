c7803d37892781442d6db5b55024357ca44724ae
Olha Tarasova <tarasovao0505@gmail.com>
2021-07-05 12:30:07 +0300

    Добавлены условия задач

--- js/task-05.js
+++ js/task-05.js
@@ -1,3 +1,7 @@
+//Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input),
+//подставляет его текущее значение в span#name-output. Если инпут пустой, в спане должна
+//отображаться строка 'незнакомец'.
+
 const inputEl = document.querySelector("#name-input");
 const outputEl = document.querySelector("#name-output");
 
--- js/task-07.js
+++ js/task-07.js
@@ -1,3 +1,7 @@
+//  Напиши скрипт, который реагирует на изменение значения input#font-size-control
+//(событие input) и изменяет инлайн-стиль span#text обновляя свойство font-size.
+//В результате при перетаскивании ползунка будет меняться размер текста.
+
 const inputFontSizeEl = document.querySelector("#font-size-control");
 const inputText = document.querySelector("#text");
 
